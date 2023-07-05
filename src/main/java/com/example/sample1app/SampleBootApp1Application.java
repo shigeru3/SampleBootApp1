a2f00e3cc4bc4f7b3d65e6c8363760d3c7d26e6a
shigeru <hironoki3@gmail.com>
2023-07-05 20:23:12 +0900

    REST Controller Parameter Variable

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -4,17 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
 @RestController
 public class SampleBootApp1Application {
 
+	String[][] data = {
+			{"noname", "no email address", "0"},
+			{"taro", "taro@yamada", "39"},
+			{"jiro", "joro@tanaka", "28"},
+			{"saburo", "saburo@sato", "17"},
+			{"shiro", "shiro@suzuki", "6"}
+	};
+
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
 	}
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello, Spring Boot 3!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int n = num < 0 ? 0 : num >= data.length ? 0 : num;
+		String[] item = data[n];
+		String msg = "ID=%s. {name: %s, mail: %s, age: %s}";
+		return String.format(msg, num, item[0], item[1], item[2]);
 	}
 }
