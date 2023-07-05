e71a080cf228e0182589425757720c3b840ca77c
shigeru <hironoki3@gmail.com>
2023-07-05 20:07:57 +0900

    GUI Application

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,6 +1,8 @@
 package com.example.sample1app;
 
-import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner.Mode;
@@ -13,16 +15,16 @@ public class SampleBootApp1Application implements ApplicationRunner {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SampleBootApp1Application.class);
 		app.setBannerMode(Mode.OFF);
+		app.setHeadless(false);
 		app.run(args);
 	}
 
 	@Override
 	public void run(ApplicationArguments args) {
-		System.out.println("+-------------------------------------+");
-		System.out.println("| this is CommandLine Runner program. |");
-		System.out.println("+-------------------------------------+");
-		System.out.println(args.getOptionNames());
-		System.out.println(args.getNonOptionArgs());
-		System.out.println(Arrays.asList(args.getSourceArgs()));
+		JFrame frame = new JFrame("Spring Boot Swing Application");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 200);
+		frame.add(new JLabel("Spring Boot Application"));
+		frame.setVisible(true);
 	}
 }
