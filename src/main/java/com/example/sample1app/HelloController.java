2a8dc38e1a45516955c2070b6dc220b575f0fa6b
shigeru <hironoki3@gmail.com>
2023-07-06 16:17:10 +0900

    Controller path variable

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +9,15 @@ public class HelloController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+
+	@RequestMapping("/{temp}")
+	public String index(@PathVariable String temp) {
+		switch (temp) {
+			case "index":
+				return "index";
+			default:
+				return "other";
+		}
 	}
 }
