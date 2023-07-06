500e376a3821d0b632a591ff064ee1b5048d9ecf
shigeru <hironoki3@gmail.com>
2023-07-06 16:55:56 +0900

    Controller model and view form

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,18 +2,23 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/{num}")
-	public ModelAndView index(@PathVariable int num,  ModelAndView mav) {
-		int res = 0;
-		for (int i = 1; i <= num; i++) {
-			res += i;
-		}
-		mav.addObject("msg", "Total: " + res);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg", "Write your name");
+		mav.setViewName("index");
+		return mav;
+	}
+
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public ModelAndView form(@RequestParam("text1") String str, ModelAndView mav) {
+		mav.addObject("msg", "Hello " + str);
+		mav.addObject("value", str);
 		mav.setViewName("index");
 		return mav;
 	}
