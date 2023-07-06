16d2a1d7d12220449800ae54f47a9992de91c3f8
shigeru <hironoki3@gmail.com>
2023-07-06 16:22:27 +0900

    Controller model

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,13 +1,15 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("msg", "This is message for index page");
 		return "index";
 	}
 
