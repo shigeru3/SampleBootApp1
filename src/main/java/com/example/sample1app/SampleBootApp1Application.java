bb71306afaf48d1849417d92a6a46bfcf9e7dcfc
shigeru <hironoki3@gmail.com>
2023-07-06 15:58:18 +0900

    Controller thymeleaf

--- src/main/java/com/example/sample1app/SampleBootApp1Application.java
+++ src/main/java/com/example/sample1app/SampleBootApp1Application.java
@@ -1,12 +1,7 @@
 package com.example.sample1app;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,22 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleBootApp1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleBootApp1Application.class, args);
-	}
-
-	@RequestMapping("/")
-	public String index(
-			HttpServletRequest request,
-			HttpServletResponse response) {
-		response.setContentType(MediaType.TEXT_HTML_VALUE);
-		String content = """
-				<html>
-					<head><title>Sample Application</title></head>
-					<body>
-						<h1>Sample Application</h1>
-						<p>This is sample app HTML page</p>
-					</body>
-				</html>
-				""";
-		return content;
 	}
 }
