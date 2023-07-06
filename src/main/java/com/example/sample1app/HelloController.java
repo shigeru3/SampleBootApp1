734eb42f59dcbd5ce0a665c973d7d2db0b1bf8a5
shigeru <hironoki3@gmail.com>
2023-07-06 17:53:02 +0900

    Thymeleaf if unless

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,25 +2,18 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	private boolean flag = false;
+
+	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
+		flag = !flag;
 		mav.addObject("msg", "HelloController.index()");
+		mav.addObject("flag", flag);
 		mav.setViewName("index");
 		return mav;
-	}
-
-	@RequestMapping(value = "/other")
-	public String other() {
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "/home")
-	public String home() {
-		return "forward:/";
 	}
 }
