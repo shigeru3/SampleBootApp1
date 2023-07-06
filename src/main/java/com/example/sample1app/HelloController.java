c59392e980d33903f033abbfee91f4ab0bf38c6e
shigeru <hironoki3@gmail.com>
2023-07-06 18:11:00 +0900

    Thymeleaf switch

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,15 +2,15 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping(value = "/")
-	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("msg", "display data");
-		String[] data = new String[] {"One", "Two", "Three"};
-		mav.addObject("data", data);
+	@RequestMapping(value = "/{month}")
+	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
+		mav.addObject("msg", "month: " + month);
+		mav.addObject("month", month);
 		mav.setViewName("index");
 		return mav;
 	}
