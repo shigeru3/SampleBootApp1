b68f44ec204e00657d380de22b9a38f7cb020627
shigeru <hironoki3@gmail.com>
2023-07-07 18:43:46 +0900

    Groovy template

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -3,8 +3,6 @@ package com.example.sample1app;
 import java.io.IOException;
 import java.io.Writer;
 
-import com.samskivert.mustache.Mustache.Lambda;
-import com.samskivert.mustache.Template.Fragment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,19 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
-		mav.addObject("title", "Lambda sample");
-		mav.addObject("msg", "This message is wrote by using Lambda");
-
-		Lambda fn = new Lambda() {
-			@Override
-			public void execute(Fragment frag, Writer out) throws IOException {
-				out.write("<div class=\"alert alert-primary\">");
-				frag.execute(out);
-				out.write("</div>");
-			}
-		};
-		mav.addObject("fn", fn);
-
+		mav.addObject("title", "Groovy templates");
+		mav.addObject("msg", "This is sample message");
 		mav.setViewName("index");
 		return mav;
 	}
