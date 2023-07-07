da1e5bf6962052c16ea60a6df6d497defc8a8dcd
shigeru <hironoki3@gmail.com>
2023-07-07 17:23:09 +0900

    Mustache template

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -2,15 +2,13 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	@RequestMapping(value = "/{month}")
-	public ModelAndView index(@PathVariable int month, ModelAndView mav) {
-		mav.addObject("msg", "month: " + month);
-		mav.addObject("month", month);
+	@RequestMapping(value = "/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("msg", "This is message.");
 		mav.setViewName("index");
 		return mav;
 	}
