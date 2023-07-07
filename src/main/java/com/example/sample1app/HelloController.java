ccab52fd0c11c4a833c44b56ecbc11d9b248dfcb
shigeru <hironoki3@gmail.com>
2023-07-07 19:00:13 +0900

    Groovy template if else

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,18 +1,18 @@
 package com.example.sample1app;
 
-import java.io.IOException;
-import java.io.Writer;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+	private boolean flag = false;
 	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
+		flag = !flag;
 		mav.addObject("title", "Groovy templates");
 		mav.addObject("msg", "This is sample message");
+		mav.addObject("flag", flag);
 		mav.setViewName("index");
 		return mav;
 	}
