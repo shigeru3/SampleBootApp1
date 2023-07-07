4f8f97ca01d4ac8da61745748a9506a14f2b5e3d
shigeru <hironoki3@gmail.com>
2023-07-07 18:03:22 +0900

    Mustache Lambda

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -1,35 +1,32 @@
 package com.example.sample1app;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import com.samskivert.mustache.Mustache.Lambda;
+import com.samskivert.mustache.Template.Fragment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	private boolean flag = false;
 	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
-		MyData[] data = new MyData[] {
-				new MyData("Taro", 39),
-				new MyData("Jiro", 37),
-				new MyData("Saburo", 33)
+		mav.addObject("title", "Lambda sample");
+		mav.addObject("msg", "This message is wrote by using Lambda");
+
+		Lambda fn = new Lambda() {
+			@Override
+			public void execute(Fragment frag, Writer out) throws IOException {
+				out.write("<div class=\"alert alert-primary\">");
+				frag.execute(out);
+				out.write("</div>");
+			}
 		};
-		mav.addObject("data", data);
+		mav.addObject("fn", fn);
+
 		mav.setViewName("index");
 		return mav;
-	}
-}
-
-class MyData {
-	public String name;
-	public int age;
-
-	public MyData(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-
-	public String toString() {
-		return String.format("{Name: %s, age: %s}", name, age);
 	}
 }
