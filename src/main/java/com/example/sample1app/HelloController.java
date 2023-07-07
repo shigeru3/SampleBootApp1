1fd7c28ccf97966335412cc05552886784dc3c3e
shigeru <hironoki3@gmail.com>
2023-07-07 19:08:58 +0900

    Groovy template each

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -6,13 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	private boolean flag = false;
 	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
-		flag = !flag;
+		String[] data = {"Windows", "macOS", "Linux", "ChromeOS"};
 		mav.addObject("title", "Groovy templates");
 		mav.addObject("msg", "This is sample message");
-		mav.addObject("flag", flag);
+		mav.addObject("data", data);
 		mav.setViewName("index");
 		return mav;
 	}
