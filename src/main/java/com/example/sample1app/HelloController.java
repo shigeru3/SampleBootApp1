1c48e8dd22f53ee63885d26789507df19cf65338
shigeru <hironoki3@gmail.com>
2023-07-07 17:54:18 +0900

    Mustache foreach

--- src/main/java/com/example/sample1app/HelloController.java
+++ src/main/java/com/example/sample1app/HelloController.java
@@ -9,10 +9,27 @@ public class HelloController {
 	private boolean flag = false;
 	@RequestMapping(value = "/")
 	public ModelAndView index(ModelAndView mav) {
-		flag = !flag;
-		mav.addObject("flag", flag);
-		mav.addObject("msg", "Display message");
+		MyData[] data = new MyData[] {
+				new MyData("Taro", 39),
+				new MyData("Jiro", 37),
+				new MyData("Saburo", 33)
+		};
+		mav.addObject("data", data);
 		mav.setViewName("index");
 		return mav;
+	}
+}
+
+class MyData {
+	public String name;
+	public int age;
+
+	public MyData(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString() {
+		return String.format("{Name: %s, age: %s}", name, age);
 	}
 }
